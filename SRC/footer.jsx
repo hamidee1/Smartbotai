795a003959f944054823eb597521d402a7d0d455
hamidee1 <mohaaymane091@gmail.com>
2025-09-03 20:24:17 +0100

    Create footer.jsx

---
+++ SRC/footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="bg-white/70 backdrop-blur-md shadow-inner py-4 text-center text-gray-600 mt-6">
+      <p>© 2025 SmartBot AI. جميع الحقوق محفوظة.</p>
+    </footer>
+  );
+}
